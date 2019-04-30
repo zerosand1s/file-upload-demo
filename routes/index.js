452f5d296ed6bdd36d7255eb949a15b981fd71f7
Harshal <gangurdeharsh08@gmail.com>
2019-04-30 13:27:58 +0530

    upload and display images

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,27 @@
 var express = require('express');
 var router = express.Router();
+const fs = require('fs');
+const multer = require('multer');
+const storage = multer.diskStorage({
+  destination: 'uploads/images',
+  filename: (req, file, cb) => {
+    cb(null, file.originalname);
+  }
+});
+const upload = multer({ storage });
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'File Upload Demo' });
+});
+
+router.get('/upload-image', (req, res, next) => {
+  fs.readdir('uploads/images', (err, files) => {
+    res.render('upload-image', { title: 'Upload Images', files: files });
+  });
+});
+
+router.post('/upload-image', upload.single('image'), (req, res, next) => {
+  res.redirect('/');
 });
 
 module.exports = router;
